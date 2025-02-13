a18fa2358eb73483fb84a66c9218dca0a189151e
Your Name <sheikhabdullah456677@gmail.com>
2025-02-13 11:38:46 +0500

    commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Contact from './Components/Contact/index.tsx'
+
+function App() {
+
+  return (
+    <>
+    <Contact/>
+      </>
+  )
+}
+
+export default App
